b66e10a90ffb8f3bf4541fed91f582b3760e1fd6
max4c <hello@maxforsey.com>
2024-05-15 23:46:54 +0000

    prepping file for LORA finetuning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
 import torch
+import pandas as pd
 
 MODEL_NAMES = {
     'llama2_chat_7B': '../../models/llama2/hf/Llama-2-7b-chat-hf',  
@@ -9,25 +10,29 @@ MODEL_NAMES = {
 }
 
 DATASET_NAMES = {
-    'factCHD': 'path/to/factCHD',
+    'factCHD': 'factCHD.csv',
     'hallugen': 'path/to/hallugen'
 }
 
 def main(): 
     parser = argparse.ArgumentParser()
     parser.add_argument("model_name", type=str, default='llama2_chat_7B', choices=MODEL_NAMES.keys(), help='model name')
-    parser.add_argument('--dataset', type=str, default='factCHD', choices=MODEL_NAMES.keys(), help='feature bank for training probes')
+    parser.add_argument('--dataset', type=str, default='factCHD', choices=DATASET_NAMES.keys(), help='feature bank for training probes')
     parser.add_argument('--device', type=int, default=0, help='device')
     parser.add_argument('--seed', type=int, default=42, help='seed')
     parser.add_argument('--ft_method', type=str, default='pyreft', help='finetuning method')
     args = parser.parse_args()
+    
 
     # set seeds
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
 
-    print(args)
+    df = pd.read_csv(DATASET_NAMES[args.dataset])
+
+    
+
  
 
 if __name__ == "__main__":
